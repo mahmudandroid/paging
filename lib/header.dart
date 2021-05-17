f34504bc1afcc2aed0a7de7e9c70d6c41d7401ff
mahmudandroid <mnorelhadi@gmail.com>
2021-05-17 10:57:04 +0200

    update it

--- lib/header.dart
+++ lib/header.dart
@@ -1,13 +1,9 @@
-import 'dart:convert';
-
-
 
 class Header {
 
 
   getHeaderToken(var token)  {
     Map<String,String> headers = {'Content-Type':'application/json' , 'Consumer' : '254321889'  ,'Authorization': 'Bearer $token'};
-
     return headers;
   }
 }
